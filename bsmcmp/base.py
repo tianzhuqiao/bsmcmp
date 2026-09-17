Fix a bug in handling none numerical data in netCDF files.

bsmcmp/base.py
@@ -5,6 +5,7 @@ import re
 import functools
 from pathlib import Path
 import numpy as np
+import numpy.ma as ma
 import click
 from click.core import ParameterSource
 import yaml
@@ -274,6 +275,8 @@ class TestBaseGroup(TestBase):
             if not d1.shape:
                 # empty variable
                 match = True
+            elif ma.is_masked(d1) and ma.is_masked(d2):
+                match = ma.allequal(d1, d2)
             else:
                 # equal_nan is not supported for non-numeric data type
                 match = np.array_equal(d1, d2, equal_nan=np.issubdtype(d1.dtype, np.number))
@@ -284,18 +287,21 @@ class TestBaseGroup(TestBase):
             self.error(f"{indent}{name}: ", fg=None, nl=False)
             self.error("fail")
             if d1.shape == d2.shape:
-                err = np.abs(d1 - d2)
-                w = np.argwhere(err == np.max(err))
-                n_zero_err = np.sum((err.flatten() == 0))
-                n_all = len(err.flatten())
+                if np.issubdtype(d1.dtype, np.number) and np.issubdtype(d2.dtype, np.number):
+                    err = np.abs(d1 - d2)
+                    w = np.argwhere(err == np.max(err))
+                    n_zero_err = np.sum((err.flatten() == 0))
+                    n_all = len(err.flatten())
 
-                self.error(f"{indent}    max error: {np.nanmax(err):.6g} at", fg=None)
-                self.error(f"{indent}              " + str(w).replace('\n', f'\n{indent}              '), fg=None)
-                self.error(f"{indent}           d1: {d1[tuple(w[0])]}", fg=None)
-                self.error(f"{indent}           d2: {d2[tuple(w[0])]}", fg=None)
-                self.error(f"{indent}    avg error: {np.nanmean(err):.6g}", fg=None)
-                self.error(f"{indent}    std error: {np.nanstd(err):.6g}", fg=None)
-                self.error(f"{indent}      0 error: {n_zero_err/n_all*100:.4f}% ({n_zero_err}/{n_all})", fg=None)
+                    self.error(f"{indent}    max error: {np.nanmax(err):.6g} at", fg=None)
+                    self.error(f"{indent}              " + str(w).replace('\n', f'\n{indent}              '), fg=None)
+                    self.error(f"{indent}           d1: {d1[tuple(w[0])]}", fg=None)
+                    self.error(f"{indent}           d2: {d2[tuple(w[0])]}", fg=None)
+                    self.error(f"{indent}    avg error: {np.nanmean(err):.6g}", fg=None)
+                    self.error(f"{indent}    std error: {np.nanstd(err):.6g}", fg=None)
+                    self.error(f"{indent}      0 error: {n_zero_err/n_all*100:.4f}% ({n_zero_err}/{n_all})", fg=None)
+                else:
+                    np.error(f"{indent}    not numeric", fg=None)
             else:
                 self.error(f"{indent}    d1.shape: {d1.shape}", fg=None)
                 self.error(f"{indent}    d2.shape: {d2.shape}", fg=None)

bsmcmp/netcdf.py
@@ -15,7 +15,10 @@ class TestNetcdf(TestBaseAttr):
         return attrs
 
     def get_data(self, d):
-        return np.asarray(d[:].astype(np.float64).filled(np.nan))
+        try:
+            return np.asarray(d[:].astype(np.float64).filled(np.nan))
+        except:
+            return d[:]
 
     def check_group(self, group1, group2, indent=""):
 